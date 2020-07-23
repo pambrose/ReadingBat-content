99933df8c7a17bdb0d1bb745b3a3d1458ccd6da6
Megan Leich <mleich@athenian.org>
2020-07-23 20:52:10 +0000

    create greater_than task

---
+++ python/group1/greater_than1.py
@@ -0,0 +1,13 @@
+def greater_than(num):
+    x = (num > 5)
+    return x
+
+
+def main():
+    print(greater_than(3))
+    print(greater_than(5))
+    print(greater_than(7))
+
+
+if __name__ == '__main__':
+    main()
