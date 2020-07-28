f70f5d0cf3087cfec66d4008de72015389130f52
Megan Leich <mleich@athenian.org>
2020-07-28 01:38:03 +0000

    Add greater_than2

---
+++ python/group1/greater_than2.py
@@ -0,0 +1,12 @@
+def greater_than(x, y):
+    return x > y
+
+
+def main():
+    print(greater_than(3, 5))
+    print(greater_than(5, 4))
+    print(greater_than(15, 15))
+
+
+if __name__ == '__main__':
+    main()
