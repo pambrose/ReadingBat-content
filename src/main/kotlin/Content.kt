46db6601fdf35edb9edc613df7b6053f91b8a62e
Megan Leich <mleich@athenian.org>
2020-07-28 02:41:36 +0000

    fix error in Content.kt

--- src/main/kotlin/Content.kt
+++ src/main/kotlin/Content.kt
@@ -35,7 +35,7 @@ val content =
       group("Variable Types") {
         packageName = "variable_type"
         description = "Identify basic variable types."
-        includeFilesWithType = "variable_type**.py" returns IntType
+        includeFilesWithType = "variable_type*.py" returns IntType
       }
     }
 
