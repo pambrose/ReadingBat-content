4c27c22bb697601db4ee662aa7a675a500959013
Megan Leich <mleich@athenian.org>
2020-07-30 03:21:12 +0000

    update variable_type1

--- python/variable_type/variable_type1.py
+++ python/variable_type/variable_type1.py
@@ -1,10 +1,16 @@
 def what_type(g):
-    return type(g)
-
+    x = str(type(g))
+    if "int" in x:
+        return "int"
+    elif "str" in x:
+        return "str"
+    elif "bool" in x:
+        return "bool"
+    
 
 def main():
-    print(what_type(3))
-    print(what_type(5))
+    print(what_type("cat"))
+    print(what_type(True))
     print(what_type(7))
 
 
