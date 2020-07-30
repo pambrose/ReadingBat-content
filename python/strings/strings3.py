3ee2defdbcaed27ab2f68f9b8aa09d99c3d930f9
Megan Leich <mleich@athenian.org>
2020-07-30 20:21:24 +0000

    added strings3

---
+++ python/strings/strings3.py
@@ -0,0 +1,16 @@
+# @desc Converting string cases
+
+def name(a):
+    f = a.title()
+    return f
+
+
+def main():
+    print(name('Anna'))
+    print(name('MIGUEL'))
+    print(name('FRed'))
+    print(name('Mr. Rodgers'))
+    print(name('oliVia'))
+
+if __name__ == '__main__':
+    main()
